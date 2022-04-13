367a78185e098987e660dba6e4ac9dab1d761aa6
Chiara <chiara@Chiaras-MacBook-Air.local>
2022-04-13 21:17:12 +0200

    add pytest

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -3,11 +3,9 @@ import pandas as pd
 from data_loader.data_loader import DataLoader
 
 
-class DataLoaderTest(unittest.TestCase):
+def test_data_loader():
 
-    def test_data_loader(self):
-
-        data_loader = DataLoader()
-        df = data_loader.get_data()
-        df_test_result = pd.DataFrame([[1,2,3], [4,5,6]], columns = ['a', 'b','c'])
-        self.assertEqual(df.mean().mean(), df_test_result.mean().mean())
+    data_loader = DataLoader()
+    df = data_loader.get_data()
+    df_test_result = pd.DataFrame([[1,2,3], [4,5,6]], columns = ['a', 'b','c'])
+    assert df.mean().mean() == df_test_result.mean().mean()
